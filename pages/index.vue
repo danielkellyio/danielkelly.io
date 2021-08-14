c25bbbab2eb9a631f4019ca3cade74c0d998cc1f
Daniel Kelly <me@danielkelly.io>
2021-08-14 07:41:37 -0500

    overflow hidden

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center"
+    class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center overflow-x-hidden overflow-y-hidden"
   >
     <div class="p-5 relative z-10 text-center">
       <img
